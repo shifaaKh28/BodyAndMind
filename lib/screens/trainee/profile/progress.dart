41927b8bc25467616572d7ae0fac6aeb1e7eceab
shifaa_Khatib <132808634+shifaaKh28@users.noreply.github.com>
2024-12-04 15:22:56 +0200

    Update progress.dart

--- lib/screens/trainee/profile/progress.dart
+++ lib/screens/trainee/profile/progress.dart
@@ -5,11 +5,29 @@ class ProgressScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Progress Tracker'),
+        title: Text('Progress'),
         backgroundColor: Colors.blueAccent,
       ),
-      body: Center(
-        child: Text('Progress Tracker Screen', style: TextStyle(fontSize: 20)),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                'Progress Graphs and Stats will be displayed here.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              // Navigate to Add Progress Screen
+            },
+            icon: Icon(Icons.add_chart),
+            label: Text('Add Progress'),
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent),
+          ),
+        ],
       ),
     );
   }
