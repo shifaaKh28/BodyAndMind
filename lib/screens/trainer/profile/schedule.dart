bb164092a2801d2c1c6d4bbf081ac153844f63e6
shifaa_Khatib <132808634+shifaaKh28@users.noreply.github.com>
2024-12-04 13:51:23 +0200

    Update schedule.dart

---
+++ lib/screens/trainer/profile/schedule.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TrainerSchedule extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Schedule'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: Text(
+          'Trainer Schedule Screen Coming Soon!',
+          style: TextStyle(fontSize: 18, color: Colors.grey),
+        ),
+      ),
+    );
+  }
+}
