2a0bd450efcdb7583b2bdfff17d37be95eb2f41a
SalehSawaed1 <salehsawaed16@gmail.com>
2025-02-23 11:55:07 +0200

    Update login.dart

--- lib/screens/trainer/login.dart
+++ lib/screens/trainer/login.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import '../common/facebook_auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'dashboard.dart';
+import '../common/google_auth_service.dart'; // Import GoogleAuthService
+
 
 class TrainerLoginScreen extends StatefulWidget {
   @override
@@ -11,6 +14,7 @@ class TrainerLoginScreen extends StatefulWidget {
 class _TrainerLoginScreenState extends State<TrainerLoginScreen> {
   final _emailOrPhoneController = TextEditingController();
   final _passwordController = TextEditingController();
+  final GoogleAuthService _googleAuthService = GoogleAuthService(); // Google Auth Service
   bool _isLoading = false;
 
   // Determine if input is phone or email
@@ -76,6 +80,90 @@ class _TrainerLoginScreenState extends State<TrainerLoginScreen> {
     }
   }
 
+  Future<void> _loginWithGoogle() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final userCredential = await _googleAuthService.signInWithGoogle();
+      if (userCredential != null) {
+        final uid = userCredential.user!.uid;
+
+        // Check if the user exists in the 'trainees' collection
+        final traineeDoc = await FirebaseFirestore.instance
+            .collection('trainers')
+            .doc(uid)
+            .get();
+
+        if (traineeDoc.exists) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Welcome, ${traineeDoc['name']}!')),
+          );
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => TrainerDashboard()),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('You are not registered as a trainer.')),
+          );
+          FirebaseAuth.instance.signOut();
+        }
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to sign in with Google: $e')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _loginWithFacebook() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final FacebookAuthService facebookAuthService = FacebookAuthService();
+      final user = await facebookAuthService.signInWithFacebook();
+
+      if (user != null) {
+        // Check if the user exists in the 'trainees' collection
+        final traineeDoc = await FirebaseFirestore.instance
+            .collection('trainers')
+            .doc(user.uid)
+            .get();
+
+        if (traineeDoc.exists) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Welcome, ${traineeDoc['name']}!')),
+          );
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => TrainerDashboard()),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('You are not registered as a trainer.')),
+          );
+          FirebaseAuth.instance.signOut();
+        }
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Facebook Login failed: $e')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   Future<void> _checkTrainer(String uid) async {
     DocumentSnapshot userDoc =
     await FirebaseFirestore.instance.collection('trainers').doc(uid).get();
@@ -123,7 +211,7 @@ class _TrainerLoginScreenState extends State<TrainerLoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // Dark background
+      backgroundColor: Colors.black,
       body: Center(
         child: SingleChildScrollView(
           child: Padding(
@@ -131,7 +219,7 @@ class _TrainerLoginScreenState extends State<TrainerLoginScreen> {
             child: Container(
               padding: EdgeInsets.all(20.0),
               decoration: BoxDecoration(
-                color: Colors.grey[900], // Dark container background
+                color: Colors.grey[900],
                 borderRadius: BorderRadius.circular(20),
                 boxShadow: [
                   BoxShadow(
@@ -143,88 +231,61 @@ class _TrainerLoginScreenState extends State<TrainerLoginScreen> {
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
                 children: [
-                  // Title
                   Center(
                     child: Text(
                       'Trainer Login',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.orange,
-                      ),
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.orange),
                     ),
                   ),
                   SizedBox(height: 20),
-
-                  // Email/Phone Field
-                  _buildDarkTextField(
-                    controller: _emailOrPhoneController,
-                    label: 'Email or Phone',
-                    icon: Icons.person_outline,
-                  ),
+                  _buildDarkTextField(controller: _emailOrPhoneController, label: 'Email or Phone', icon: Icons.person_outline),
                   SizedBox(height: 16),
-
-                  // Password Field
-                  _buildDarkTextField(
-                    controller: _passwordController,
-                    label: 'Password',
-                    icon: Icons.lock_outline,
-                    obscureText: true,
-                  ),
+                  _buildDarkTextField(controller: _passwordController, label: 'Password', icon: Icons.lock_outline, obscureText: true),
                   SizedBox(height: 24),
 
-                  // Login Button
                   Center(
                     child: ElevatedButton(
                       onPressed: _isLoading ? null : _loginTrainer,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.orange,
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 50, vertical: 15),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                      child: _isLoading
-                          ? CircularProgressIndicator(color: Colors.white)
-                          : Text(
-                        'Login',
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+                      child: _isLoading ? CircularProgressIndicator(color: Colors.white) : Text('Login', style: TextStyle(color: Colors.white)),
                     ),
                   ),
+                  SizedBox(height: 16),
+
+                  Center(
+                    child: ElevatedButton.icon(
+                      onPressed: _isLoading ? null : _loginWithGoogle,
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                      icon: Icon(Icons.g_mobiledata, color: Colors.white),
+                      label: Text('Login with Google', style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+
+                  Center(
+                    child: ElevatedButton.icon(
+                      onPressed: _isLoading ? null : _loginWithFacebook,
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue[800]),
+                      icon: Icon(Icons.facebook, color: Colors.white),
+                      label: Text('Login with Facebook', style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+
                   SizedBox(height: 10),
 
-                  // Forgot Password
                   Center(
                     child: TextButton(
                       onPressed: _resetPassword,
-                      child: Text(
-                        'Forgot Password?',
-                        style: TextStyle(
-                          color: Colors.orange,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                      child: Text('Forgot Password?', style: TextStyle(color: Colors.orange)),
                     ),
                   ),
-                  SizedBox(height: 10),
 
-                  // Register Option
                   Center(
                     child: TextButton(
                       onPressed: () {
                         Navigator.pushNamed(context, '/trainerRegister');
                       },
-                      child: Text(
-                        'Don’t have an account? Register here',
-                        style: TextStyle(
-                          color: Colors.orange,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                      child: Text('Don’t have an account? Register here', style: TextStyle(color: Colors.orange)),
                     ),
                   ),
                 ],
